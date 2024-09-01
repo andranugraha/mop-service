a3f48030752e9cf5a3b00cdbd7e345e4da5b4229
Rizky Muhammad Baihaqy <73181490+rizkybaihaqy@users.noreply.github.com>
2024-09-01 12:27:25 +0700

    Refactor landing handler to include landing request in error response

--- internal/handler/landing/landing.go
+++ internal/handler/landing/landing.go
@@ -3,19 +3,21 @@ package landing
 import (
 	"github.com/empnefsi/mop-service/internal/common/constant"
 	"github.com/empnefsi/mop-service/internal/common/response"
+	"github.com/empnefsi/mop-service/internal/dto/landing"
 	"github.com/gofiber/fiber/v2"
 )
 
 func (h *impl) Landing(c *fiber.Ctx) error {
 	code := c.Params("code")
+	req := &landing.LandingRequest{Code: code}
 
 	if code == "" {
-		return response.Error(c, constant.ErrCodeInvalidParam, "Code is required")
+		return response.Error(c, req, constant.ErrCodeInvalidParam, "Code is required")
 	}
 
 	data, err := h.manager.Landing(c.UserContext(), code)
 	if err != nil {
-		return response.Error(c, constant.ErrCodeInternalServer, err.Error())
+		return response.Error(c, req, constant.GetErrorCode(err), err.Error())
 	}
 
 	return response.Success(c, code, data)
